9d52215f4e0d8bfd13f42bc46fb5cc9351498527
guusdias <gustavohd34@gmail.com>
2024-08-08 16:42:06 -0300

    fix: product card styles

--- app/ui/home/products/product-card.tsx
+++ app/ui/home/products/product-card.tsx
@@ -9,25 +9,29 @@ interface ProductCardProps {
 
 export default function ProductCard({ products }: ProductCardProps) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {products.map((product) => (
         <Link
           key={product.productId}
-          className="border p-4 rounded-lg flex flex-col h-96 justify-between self-center w-80"
           href={`/home/products/${product.productId}`}
+          className="border p-4 rounded-lg flex flex-col h-full justify-between shadow-sm hover:shadow-lg transition-shadow duration-200"
         >
-          <Image
-            alt={product.title.text}
-            src={product.productImage.url}
-            width={200}
-            height={200}
-            className="object-cover self-center items-center justify-center mb-7"
-          />
-          <div className="items-start">
+          <div className="flex justify-center">
+            <Image
+              alt={product.title.text}
+              src={product.productImage.url}
+              width={200}
+              height={200}
+              className="object-contain"
+            />
+          </div>
+          <div className="mt-4 text-center flex flex-col items-start justify-center">
             <h2 className="text-lg font-semibold">{product.title.text}</h2>
-            <p className="text-gray-700">{product.description}</p>
-            <p className="text-lg font-bold text-slate-600">${product.price}</p>
-            <button className="bg-slate-500 text-white px-4 py-2 rounded mt-4">
+            <p className="text-gray-700 mt-1">{product.description}</p>
+            <p className="text-lg font-bold text-slate-600 mt-2">
+              ${product.price}
+            </p>
+            <button className="bg-slate-500 text-white px-4 py-2 rounded mt-4 hover:bg-slate-600 transition-colors duration-200 w-40">
               Add to Cart
             </button>
           </div>
